c3c18c77c1e8c39accbb40a98e2be42eec370c9b
Ollie Walsh <ollie.walsh@gmail.com>
2015-08-21 15:13:49 +0100

    Option to specify setting module in env var

--- saltpad/settings.py
+++ saltpad/settings.py
@@ -1,6 +1,17 @@
+import os 
 from .default_settings import *
 
 try:
     from .local_settings import *
 except ImportError:
     pass
+
+_env_settings = os.environ.get('SALTPAD_SETTINGS_MODULE') 
+if _env_settings is not None:
+    globals().update(
+        __import__(
+            _env_settings,
+            {},
+            {}
+        ).__dict__
+    )
